8997c65714e90e68149bb98b8d29da509ddcb275
sajanchahal <113524537+sajanchahal@users.noreply.github.com>
2023-11-13 07:43:39 +0530

    Update live.sh

--- live.sh
+++ live.sh
@@ -1,19 +1,18 @@
 #! /bin/bash
 #
-# Diffusion youtube avec ffmpeg
+# YouTube streaming with FFmpeg
 
-# Configurer youtube avec une résolution 720p. La vidéo n'est pas scalée.
+VBR="2500k"                                    # Output video bitrate
+FPS="30"                                       # Output video FPS
+QUAL="medium"                                  # FFmpeg quality preset
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"  # YouTube RTMP base URL
 
-VBR="2500k"                                    # Bitrate de la vidéo en sortie
-FPS="30"                                       # FPS de la vidéo en sortie
-QUAL="medium"                                  # Preset de qualité FFMPEG
-YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"  # URL de base RTMP youtube
-
-SOURCE="20230813_201849.mp4"              # Source UDP (voir les annonces SAP)
-KEY="zkae-3zte-ee9s-yv6r-942w"                                     # Clé à récupérer sur l'event youtube
+SOURCE="20230813_201849.mp4"                   # UDP source (see SAP announcements)
+KEY="zkae-3zte-ee9s-yv6r-942w"                 # Key to retrieve from YouTube event
 
 ffmpeg \
     -stream_loop -1 -i "$SOURCE" -deinterlace \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
     -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
     -f flv "$YOUTUBE_URL/$KEY"
+    
